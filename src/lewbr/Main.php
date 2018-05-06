16354db862354b4473b468768bc66a272766ab35
Leonardo S. William <minethex@gmail.com>
2018-05-06 13:47:41 -0300

    Update Main.php

--- src/lewbr/Main.php
+++ src/lewbr/Main.php
@@ -12,19 +12,19 @@ class Main extends PluginBase{
 	public $config;
 	
     public function onEnable()
-	{
+    {
 		
-		$this->getServer()->getPluginManager()->registerEvents(new OuvintePrincipal($this), $this);
+	$this->getServer()->getPluginManager()->registerEvents(new OuvintePrincipal($this), $this);
 		
         @mkdir($this->getDataFolder());
 		$this->saveDefaultConfig();
-        $this->config = new Config($this->getDataFolder() . "/config.yml", Config::YAML);
-        $this->getLogger()->info("[Título] Ativado! :)");
+        	$this->config = new Config($this->getDataFolder() . "/config.yml", Config::YAML);
+        	$this->getLogger()->info("[Título] Ativado! :)");
 		
     }
 	
     public function onDisable()
-	{
+    {
 		
         $this->getLogger()->info("[Título] Desconectado! :(");
 		
