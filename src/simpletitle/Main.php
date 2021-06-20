5f547d137ae961ae747a236b61ca0054d8624207
Leonardo Santos Jacyntho <minethex@gmail.com>
2021-06-20 13:26:25 +0200

    Update and rename src/lewbr/Main.php to src/simpletitle/Main.php

---
+++ src/simpletitle/Main.php
@@ -0,0 +1,23 @@
+<?php
+namespace simpletitle;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
+use simpletitle\events\EventListener;
+
+class Main extends PluginBase{
+	
+	public $config;
+	
+    public function onEnable(){
+	$this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
+        @mkdir($this->getDataFolder());
+		$this->saveDefaultConfig();
+        	$this->config = new Config($this->getDataFolder() . "/config.yml", Config::YAML);
+        	$this->getLogger()->info("[SimpleTitle] Enabled.");	
+    }
+	
+    public function onDisable(){
+        $this->getLogger()->info("[SimpleTitle] Disabled.");
+    }
+}
