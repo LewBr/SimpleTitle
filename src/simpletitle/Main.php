30feef326eb340efafc30f7d37d22d90b97a56c7
Leonardo Santos Jacyntho <minethex@gmail.com>
2021-06-20 14:20:05 +0200

    Update Main.php

--- src/simpletitle/Main.php
+++ src/simpletitle/Main.php
@@ -8,21 +8,15 @@ use simpletitle\event\EventListener;
 class Main extends PluginBase{
 	
     public $config;
-    private static $instance;
 	
     public function onEnable(){
 	$this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
-	self::$instance = $this;
         @mkdir($this->getDataFolder());
 		$this->saveDefaultConfig();
         	$this->config = new Config($this->getDataFolder() . "/config.yml", Config::YAML);
         	$this->getLogger()->info("[SimpleTitle] Enabled.");	
     }
 
-    public static function getInstance(): Main {
-        return self::$instance;
-    }
-	
     public function onDisable(){
         $this->getLogger()->info("[SimpleTitle] Disabled.");
     }
