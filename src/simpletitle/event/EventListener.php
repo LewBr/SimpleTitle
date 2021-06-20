16727962ee2a9e15eea9ea80434508caa6aaf7ad
Leonardo Santos Jacyntho <minethex@gmail.com>
2021-06-20 13:56:38 +0200

    Update EventListener.php

--- src/simpletitle/event/EventListener.php
+++ src/simpletitle/event/EventListener.php
@@ -6,7 +6,7 @@ use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\event\Listener;
 
 use simpletitle\Main;
-use simpletitle\task\Task;
+use simpletitle\task\TaskFile;
 
 class EventListener implements Listener{
 	public function __construct($plugin){
@@ -17,6 +17,6 @@ class EventListener implements Listener{
 		$player = $event->getPlayer();
 		$values = array_values($this->plugin->config->get("configurations"));
 		
-		$this->plugin->getServer()->getScheduler()->scheduleDelayedTask(new Task($this->plugin, $player), (int)$values[3]);
+		$this->plugin->getServer()->getScheduler()->scheduleDelayedTask(new TaskFile($this->plugin, $player), (int)$values[3]);
 	}  
 }
